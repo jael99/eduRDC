f5d5704e847709c8398c472df8448345759e3c65
jael99 <etswakajenis@gmail.com>
2026-01-11 23:00:07 +0100

    firt dev

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    nom: str
+    prenom: str
+    email: EmailStr
+    password: str
+    role: str
+
+class UserOut(BaseModel):
+    id: int
+    nom: str
+    prenom: str
+    email: EmailStr
+    role: str
+
+    class Config:
+        orm_mode = True
