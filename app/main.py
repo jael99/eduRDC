95e5c44fbedb00032caf755c6460a342a72a2528
jael99 <etswakajenis@gmail.com>
2026-01-12 15:17:12 +0100

    suite archi

--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
-from app.core.database import Base, engine
+#from app.core.database import Base, engine
 from app.routes import auth
 
-Base.metadata.create_all(bind=engine)
+#Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="EduRDC API")
 
 app.include_router(auth.router)
 
-@app.get("/")
-def root():
-    return {"message": "EduRDC Backend is running"}
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
+
 from app.core.database import get_db
 from app.models.user import User
 from app.schemas.user import UserCreate, UserOut
@@ -9,9 +11,8 @@ router = APIRouter(prefix="/auth", tags=["Auth"])
 
 @router.post("/register", response_model=UserOut)
 def register(user: UserCreate, db: Session = Depends(get_db)):
-    existing = db.query(User).filter(User.email == user.email).first()
-    if existing:
-        raise HTTPException(status_code=400, detail="Email déjà utilisé")
+    if db.query(User).filter(User.email == user.email).first():
+        raise HTTPException(400, "Email déjà utilisé")
 
     new_user = User(
         nom=user.nom,
@@ -26,10 +27,13 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 @router.post("/login")
-def login(email: str, password: str, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.email == email).first()
-    if not user or not verify_password(password, user.password):
-        raise HTTPException(status_code=401, detail="Identifiants invalides")
+def login(
+    form: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
+    user = db.query(User).filter(User.email == form.username).first()
+    if not user or not verify_password(form.password, user.password):
+        raise HTTPException(401, "Identifiants invalides")
 
-    token = create_access_token({"sub": user.email, "role": user.role})
+    token = create_access_token({"sub": str(user.id)})
     return {"access_token": token, "token_type": "bearer"}
