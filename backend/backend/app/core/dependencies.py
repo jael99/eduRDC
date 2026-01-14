8327e6d8827016f0713e49d953c2b2630e7b9e04
jael99 <etswakajenis@gmail.com>
2026-01-14 18:10:57 +0100

    fusion

--- backend/backend/app/core/dependencies.py
+++ backend/backend/app/core/dependencies.py
@@ -1,10 +1,13 @@
 from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 
 from app.core.database import get_db
-from app.core.security import SECRET_KEY, ALGORITHM, oauth2_scheme
+from app.core.security import SECRET_KEY, ALGORITHM
 from app.models.user import User
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 def get_current_user(
     token: str = Depends(oauth2_scheme),
